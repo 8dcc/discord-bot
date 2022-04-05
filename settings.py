0ad0952110c12d902c658a5bf2126b4d92a514dc
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-06 00:02:39 +0200

    v1.42

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+# Settings
+
+activityType = "Listening"
+debug = True
+
+write_to_error_log = True
+write_to_log = True
+
+discord_log_path = "logs/discord-bot.log"   # Used for logs
+bot_error_path = "logs/bot-errors.log"      # Used for errors
+config_path = "config/config.json"          # Used for some blacklists and all
