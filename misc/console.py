96b0d1e04c2a4a87ac9c9063fa3c94483c0a7962
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-05 22:59:38 +0200

    v1.39

--- misc/console.py
+++ misc/console.py
@@ -1,10 +1,8 @@
-import discord, os, time, asyncio
+import discord, os, time, asyncio, json
 from discord.ext import commands
 from dotenv import load_dotenv
-import youtube_dl
 
 ##############################
-activityType = "Watching"
 debug = True
 ##############################
 
@@ -13,10 +11,10 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix='n!', intents=intents)
-creator_name = "YOUR_NAME#1234"
 
-discord_log_path = "/your/folder/here/discord-bot.log"
-bot_error_path = "/your/folder/here/bot-errors.log"
+discord_log_path = "embed-logs/discord-bot.log"
+bot_error_path = "embed-logs/bot-errors.log"
+console_settings_path = "settings.json"
 
 # ---------------------------------------------------------------
 # Functions and initial settings
@@ -40,8 +38,11 @@ def error_print(text):
 # ---------------------------------------------------------------
 # Help command
 
-async def help_command():
-    print("No help yet fuck off")
+@commands.command()
+async def help():
+    print("Edit the configuration file for the custom_embeds!")
+
+# ---------------------------------------------------------------
 
 async def custom_embed(channel_id, title, url, color, thumbnail, description):
     color = discord.Color(value=int(color, 16))
@@ -61,41 +62,24 @@ async def on_ready():
     print("----------------------------------------------------------------")
     print("The bot %s has connected to Discord!" % client.user)
     print("----------------------------------------------------------------")
-    if activityType is "Playing":
-        await client.change_presence(activity=discord.Game(name="with your stepmom"))
-    elif activityType is "Watching":
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="lots of gay porn"))
-    elif activityType is "Listening":
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="hentai moans"))
-    else:
-        exit("activityType error. Exiting...")
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the next announcement"))
 
-    while True:
-        try:
-            command = input("> ")
-        except KeyboardInterrupt:
-            exit(1)
-        if command is "help":
-            help_command()
-        elif "custom_embed" in command:
-            channel_id = input("custom_embed - channel_id> ")
-            embed_title = input("custom_embed - title> ")
-            embed_url = input("custom_embed - url> ")
-            embed_color = input("custom_embed - color> ")
-            embed_thumbnail = input("custom_embed - thumbnail> ")
-            embed_description = input("custom_embed - description> ")
+    with open(console_settings_path, "r") as settings_file:
+        json_data = json.loads(settings_file.read())
 
-            if "0x" in embed_color:
-                embed_color = embed_color.replace("0x", "")
+        print(json.dumps(json_data, indent=4))
 
-            await custom_embed(channel_id, embed_title, embed_url, embed_color, embed_thumbnail, embed_description)
-        elif "clear" in command:
-            os.system("clear")
-        elif "exit" in command:
-            exit(1)
-        else:
-            print("Unknown command.")
+        channel_id = json_data['channel_id']
+        embed_title = json_data['embed_title']
+        embed_url = json_data['embed_url']
+        embed_color = json_data['embed_color']
+        embed_thumbnail = json_data['embed_thumbnail']
+        embed_description = json_data['embed_description']
 
+        if "0x" in embed_color:
+            embed_color = embed_color.replace("0x", "")
+
+        await custom_embed(channel_id, embed_title, embed_url, embed_color, embed_thumbnail, embed_description)
 
 # ---------------------------------------------------------------
 # Starting the bot
