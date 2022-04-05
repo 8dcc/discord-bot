12e463068a9a931a9f5908844943fa89db417c6e
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-06 00:02:18 +0200

    v1.41.5

---
+++ modules/cogs/ping.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+from modules.functions.helpers import *
+from modules.functions.checkers import *
+
+class PingCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.message.add_reaction("\U0001f44b")
+        debug_print('[Bot] User %s requested ping command.' % ctx.author)
+
+    @client.command()
+    async def memes(self, ctx):
+        embed = discord.Embed(color=0xff1111)
+        embed.set_thumbnail(url="https://u.teknik.io/UjPuB.png")
+        await ctx.send(embed=embed)
