4b81b23316baa49ead595c57784c59e866a04af6
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-07 01:43:00 +0200

    v1.45.2

--- modules/cogs/messages.py
+++ modules/cogs/messages.py
@@ -21,26 +21,28 @@ class MessagesCog(commands.Cog):
 
         if amount <= 0:
             await ctx.send(':warning: **Missing required arguments. Usage:**  `n!purge <username> <message_amount>`')
-            debug_print('[Bot] Could not parse negative integer in purge for user: %s' % ctx.author)
+            debug_print('[Bot] [W] [Messages] Could not parse negative integer in purge for user: %s' % ctx.author)
             return
 
         deleted = await ctx.channel.purge(limit=amount, check=check_purge)
 
-        embed = discord.Embed(title="Channel purged", description="**%s** removed %s messages by **%s**" % (ctx.author.display_name, len(deleted), member.display_name), color=0xff1111)
+        embed = discord.Embed(title="Channel purged", 
+                description="**%s** removed %s messages by **%s**" % (ctx.author.display_name, len(deleted), member.display_name), 
+                color=0xff1111)
         await ctx.send(embed=embed)
-        debug_print('[Bot] User %s requested purge. Deletd %s messages from user: %s' % (ctx.author, len(deleted), member))
+        debug_print('[Bot] [Messages] User %s requested purge. Deletd %s messages from user: %s' % (ctx.author, len(deleted), member))
 
     @purge.error
     async def purge_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(':warning: **Missing required arguments. Usage:**  `n!purge <username> <message_amount>`')
-            debug_print('[Bot] Could not parse arguments for user: %s' % ctx.author)
+            debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
             await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
-            debug_print('[Bot] Could not parse arguments for user: %s' % ctx.author)
+            debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
             await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
-            debug_print('[Bot] Could not parse arguments for user: %s' % ctx.author)
+            debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
         else:
             error_print(error)
 
@@ -52,10 +54,10 @@ class MessagesCog(commands.Cog):
     async def spam(self, ctx, amount : int, *, message2send : str):  #TODO
         if amount < 1:
             await ctx.send(':warning: **Missing required arguments. Usage:**  `n!spam <ammount> <message>`')
-            debug_print('[Bot] Could not parse negative integer in spam for user: %s' % ctx.author)
+            debug_print('[Bot] [E] [Messages] Could not parse negative integer in spam for user: %s' % ctx.author)
             return
 
-        debug_print('[Bot] User %s requested spam. Spamming %s times the message: %s' % (ctx.author, amount, message2send))
+        debug_print('[Bot] [Messages] User %s requested spam. Spamming %s times the message: %s' % (ctx.author, amount, message2send))
 
         for n in range(amount):
             await ctx.send(message2send)
@@ -64,10 +66,10 @@ class MessagesCog(commands.Cog):
     async def spam_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(':warning: **Missing required arguments. Usage:**  `n!spam <amount> <message>`')
-            debug_print('[Bot] Could not parse arguments for user: %s' % ctx.author)
+            debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
             await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
-            debug_print('[Bot] Could not parse arguments for user: %s' % ctx.author)
+            debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
         else:
             error_print(error)
 
