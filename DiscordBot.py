55de3c97f59d619cbd04c33882468b287a09dae0
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-07 21:23:00 +0200

    v1.50 Added n!version and n!ping to the help command

--- DiscordBot.py
+++ DiscordBot.py
@@ -69,22 +69,26 @@ client.add_cog(PingCog(client))
 @client.command()
 async def help(ctx):
 
-    help_text1 = "`n!play <url>` - Play audio in a voice channel (.mp3 url, youtube url or youtube search). \n`n!join` - Join the user's channel.\n`n!join_channel <channel_name>` - Join the specified channel.\n`n!leave` - Leaves the current channel.\n`n!pause` - Pauses the audio.\n`n!resume` - Resumes the audio.\n`n!stop` - Stops the audio without leaving the channel."
-    help_text2 = "*This commands will only work if you are the bot owner or if you are in the whitelist.*\n`n!kick @someone` to kick a user.\n`n!ban @someone` to ban a user.\n`n!mute @someone` to mute a user. Also `n!m`.\n`n!unmute @someone` to unmute a user. Also `n!um`.\n`n!deafen @someone` to deafen a user. Also `n!d`.\n`n!undeafen @someone` to undeafen a user. Also `n!ud`.\n`n!purge @someone <messages_to_check>` will check X messages, and will delete them if the author is the specified user. Also `n!clean`.\n`n!spam <amount> <message>` will spam the specified messae in the current channel the amount of times."
+    help_music = "`n!play <url>` - Play audio in a voice channel (.mp3 url, youtube url or youtube search). \n`n!join` - Join the user's channel.\n`n!join_channel <channel_name>` - Join the specified channel.\n`n!leave` - Leaves the current channel.\n`n!pause` - Pauses the audio.\n`n!resume` - Resumes the audio.\n`n!stop` - Stops the audio without leaving the channel."
+    help_misc = "`n!version` - Shows a comparison between the local commit and the remote commit. \n`n!ping` - The bot reacts to your message to make sure it is working."
+    help_admin = "*This commands will only work if you are the bot owner or if you are in the whitelist.*\n`n!kick @someone` to kick a user.\n`n!ban @someone` to ban a user.\n`n!mute @someone` to mute a user. Also `n!m`.\n`n!unmute @someone` to unmute a user. Also `n!um`.\n`n!deafen @someone` to deafen a user. Also `n!d`.\n`n!undeafen @someone` to undeafen a user. Also `n!ud`.\n`n!purge @someone <messages_to_check>` will check X messages, and will delete them if the author is the specified user. Also `n!clean`.\n`n!spam <amount> <message>` will spam the specified messae in the current channel the amount of times."
 
     embed = discord.Embed(title="Help", 
             url="https://github.com/r4v10l1/discord-bot/blob/main/README.md", 
             color=0x1111ff)
     embed.set_thumbnail(url="https://u.teknik.io/m3lTR.png")
     embed.add_field(name="Music", 
-            value=help_text1, 
+            value=help_music, 
+            inline=False)
+    embed.add_field(name="Misc", 
+            value=help_misc, 
             inline=False)
 
     isowner = await client.is_owner(ctx.author)     # Needs a variable so we can await
 
     if (isowner) or ( (int(ctx.guild.id) in client.whitelist) and (int(ctx.author.id) in client.whitelist[int(ctx.guild.id)]) ):
         embed.add_field(name="Administration",
-                value=help_text2,
+                value=help_admin,
                 inline=False)
 
     await ctx.send(embed=embed)
