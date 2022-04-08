e094e3eb288c6936aa777b263f70047025ee0565
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-08 18:17:54 +0200

    v1.51 Added commit link for the head on n!version no matter what. The head (docker) commit should be uploaded to the origin.

--- modules/cogs/ping.py
+++ modules/cogs/ping.py
@@ -39,14 +39,14 @@ class PingCog(commands.Cog):
 
         if head_hash == origin_hash:
             origin_emote = ":white_check_mark:"
-            head_description = f":whale: [`{head_hash}`](https://github.com/r4v10l1/discord-bot/commit/{head_hash})\n"
         else:
             origin_emote = ":x:"
-            head_description = f":whale: `{head_hash}`\n"
 
+        head_description = f":whale: [`{head_hash}`](https://github.com/r4v10l1/discord-bot/commit/{head_hash})\n"
+        origin_description = f"{origin_emote} [`{origin_hash}`](https://github.com/r4v10l1/discord-bot/commit/{origin_hash})",
         embed = discord.Embed(title = "Version", 
                 url = "https://github.com/r4v10l1/discord-bot/commits/main",
-                description = head_description + f"{origin_emote} [`{origin_hash}`](https://github.com/r4v10l1/discord-bot/commit/{origin_hash})",  
+                description = head_description + origin_descriptiont, 
                 color = custom_colors.DEFAULT_EMBED)
 
         await ctx.send(embed=embed)
