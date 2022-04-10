999614f9c524a3ca2adc271ebf6c0c97a8d98b48
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-10 05:02:51 +0200

    Changed chat messages in administration to embeds

--- modules/cogs/administration.py
+++ modules/cogs/administration.py
@@ -25,16 +25,32 @@ class AdministrationCog(commands.Cog):
     @kick.error
     async def kick_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!kick <username> (reason)`')
+            embed = discord.Embed(
+                    title="Missing required arguments", 
+                    description="**Usage:** `n!kick <username> (reason)`",  
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse kick arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
+            embed = discord.Embed(
+                    title="Member not found", 
+                    description="**Make sure you don't use nicknames**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse kick arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
+            embed = discord.Embed(
+                    title="Missing permissions", 
+                    description="**You don\'t have the permissions to do that", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse kick arguments for user: %s' % ctx.author)
         else:
-            await ctx.send(':warning: **I can\'t do that! %s**' % ctx.autho.mention)
+            embed = discord.Embed(
+                    title="Error", 
+                    description="I can't do that!", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             error_print(error)
 
     # ---------------------------------------------------------------
@@ -44,24 +60,41 @@ class AdministrationCog(commands.Cog):
     @commands.check_any(commands.is_owner(), check_whitelist())
     async def ban(self, ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=reason)
-        embed = discord.Embed(title="User banned", 
+        embed = discord.Embed(
+                title="User banned", 
                 description="**%s** was banned by **%s**\n**Reason:** %s" % (member.display_name, ctx.author.display_name, reason), 
-                color=0xff1111)
+                color=0x11ff11)
         await ctx.send(embed=embed)
 
     @ban.error
     async def ban_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!ban <username> (reason)`')
+            embed = discord.Embed(
+                    title="Missing required arguments", 
+                    description="**Usage:** `n!ban <username> (reason)`", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse ban arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
+            embed = discord.Embed(
+                    title="Member not found", 
+                    description="**Make sure you don\'t use nicknames**",  
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse ban arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
+            embed = discord.Embed(
+                    title="Missing permissions", 
+                    description="**You don\'t have the permissions to do that",  
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse ban arguments for user: %s' % ctx.author)
         else:
-            await ctx.send(':warning: **I can\'t do that! %s**' % ctx.autho.mention)
+            embed = discord.Embed(
+                    title="Error", 
+                    description="I can't do that!", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             error_print(error)
 
     #----------------------------------------------------------------
@@ -71,23 +104,41 @@ class AdministrationCog(commands.Cog):
     @commands.check_any(commands.is_owner(), check_whitelist())
     async def mute(self, ctx, member : discord.Member, *, reason : str = "Unknown."):
         await member.edit(mute=True)
-        embed = discord.Embed(title="User muted", 
+        embed = discord.Embed(
+                title="User muted", 
                 description="**%s** was muted by **%s**\n**Reason:** %s" % (member.display_name, ctx.author.display_name, reason), 
-                color=0xff1111)
+                color=0x11ff11)
         await ctx.send(embed=embed)
 
     @mute.error
     async def mute_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!mute <username> (reason)`')
+            embed = discord.Embed(
+                    title="Missing required arguments", 
+                    description="**Usage:** `n!mute <username> (reason)`", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
+            embed = discord.Embed(
+                    title="Member not found", 
+                    description="**Make sure you don\'t use nickname**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
+            embed = discord.Embed(
+                    title="Missing permissions", 
+                    description="**You don\'t have the permissions to do that**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         else:
+            embed = discord.Embed(
+                    title="Error", 
+                    description="I can't do that!", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             error_print(error)
 
     #----------------------------------------------------------------
@@ -97,7 +148,8 @@ class AdministrationCog(commands.Cog):
     @commands.check_any(commands.is_owner(), check_whitelist())
     async def unmute(self, ctx, *, member : discord.Member):
         await member.edit(mute=False)
-        embed = discord.Embed(title="User muted", 
+        embed = discord.Embed(
+                title="User muted", 
                 description="**%s** was unmuted by **%s**" % (member.display_name, ctx.author.display_name), 
                 color=0x11ff11)
         await ctx.send(embed=embed)
@@ -105,15 +157,32 @@ class AdministrationCog(commands.Cog):
     @unmute.error
     async def unmute_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!unmute <username>`')
+            embed = discord.Embed(
+                    title="Missing required arguments", 
+                    description="**Usage:** `n!unmute <username>`", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
+            embed = discord.Embed(
+                    title="Member not found", 
+                    description="**Make sure you don\'t use nicknames**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
+            embed = discord.Embed(
+                    title="Missing permissions", 
+                    description="**You don\'t have the permissions to do that**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         else:
+            embed = discord.Embed(
+                    title="Error", 
+                    description="I can't do that!", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             error_print(error)
 
     #----------------------------------------------------------------
@@ -123,7 +192,8 @@ class AdministrationCog(commands.Cog):
     @commands.check_any(commands.is_owner(), check_whitelist())
     async def deafen(self, ctx, member : discord.Member, *, reason : str = "Unknown."):
         await member.edit(deafen=True)
-        embed = discord.Embed(title="User deafen", 
+        embed = discord.Embed(
+                title="User deafen", 
                 description="**%s** was deafen by **%s**\n**Reason:** %s" % (member.display_name, ctx.author.display_name, reason), 
                 color=0xff1111)
         await ctx.send(embed=embed)
@@ -131,15 +201,32 @@ class AdministrationCog(commands.Cog):
     @deafen.error
     async def deafen_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!deafen <username> (reason)`')
+            embed = discord.Embed(
+                    title="Missing required arguments", 
+                    description="**Usage:** `n!deafen <username> (reason)`", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
+            embed = discord.Embed(
+                    title="Member not found", 
+                    description="**Make sure you don\'t use nicknames**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
+            embed = discord.Embed(
+                    title="Missing permissions", 
+                    description="**You don\'t have the permissions to do that**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         else:
+            embed = discord.Embed(
+                    title="Error", 
+                    description="I can't do that!", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             error_print(error)
 
 
@@ -150,7 +237,8 @@ class AdministrationCog(commands.Cog):
     @commands.check_any(commands.is_owner(), check_whitelist())
     async def undeafen(self, ctx, *, member : discord.Member):
         await member.edit(deafen=False)
-        embed = discord.Embed(title="User undeafen", 
+        embed = discord.Embed(
+                title="User undeafen", 
                 description="**%s** was undeafen by **%s**" % (member.display_name, ctx.author.display_name), 
                 color=0x11ff11)
         await ctx.send(embed=embed)
@@ -158,16 +246,32 @@ class AdministrationCog(commands.Cog):
     @undeafen.error
     async def undeafen_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!undeafen <username>`')
+            embed = discord.Embed(
+                    title="Missing required arguments", 
+                    description="**Usage:** `n!undeafen <username>`", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
+            embed = discord.Embed(
+                    title="Member not found", 
+                    description="**Make sure you don\'t use nicknames.**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
+            embed = discord.Embed(
+                    title="Missing permissions", 
+                    description="**You don\'t have the permissions to do that**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         else:
-            await ctx.send(':warning: **I can\'t do that, is the user in a channel?**')
+            embed = discord.Embed(
+                    title="Error", 
+                    description="**I can\'t do that, is the user in a channel?**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
             error_print(error)
 
@@ -178,7 +282,8 @@ class AdministrationCog(commands.Cog):
     @commands.check_any(commands.is_owner(), check_whitelist())
     async def move(self, ctx, member : discord.Member, *, channel : discord.VoiceChannel):
         await member.move_to(channel)
-        embed = discord.Embed(title="User moved", 
+        embed = discord.Embed(
+                title="User moved", 
                 description="**%s** was moved by **%s**" % (member.display_name, ctx.author.display_name), 
                 color=0xff1111)
         await ctx.send(embed=embed)
@@ -186,13 +291,25 @@ class AdministrationCog(commands.Cog):
     @move.error
     async def disconnect_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!disconnect <username> <channel>`')
+            embed = discord.Embed(
+                    title="Missing required arguments", 
+                    description="**Usage:** `n!disconnect <username> <channel>`", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
+            embed = discord.Embed(
+                    title="Member not found", 
+                    description="**Make sure you don\'t use nicknames.**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
+            embed = discord.Embed(
+                    title="Missing permissions", 
+                    description="**You don\'t have the permissions to do that**", 
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Admin] Could not parse arguments for user: %s' % ctx.author)
         else:
             error_print(error)
