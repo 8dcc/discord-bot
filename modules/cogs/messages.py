3ba840aa641a96216708f5dee9a6655c016c7983
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-15 20:40:46 +0200

    Replaced embeds in messages.py

--- modules/cogs/messages.py
+++ modules/cogs/messages.py
@@ -20,29 +20,46 @@ class MessagesCog(commands.Cog):
             return check_me.author.id == member.id
 
         if amount <= 0:
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!purge <username> <message_amount>`')
+            embed = discord.Embed(
+                    title="Missing arguments",
+                    description="**Usage:**  `n!purge <username> <message_amount>`",
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [W] [Messages] Could not parse negative integer in purge for user: %s' % ctx.author)
             return
 
         deleted = await ctx.channel.purge(limit=amount, check=check_purge)
 
-        embed = discord.Embed(title="Channel purged", 
-                description="**%s** removed %s messages by **%s**" % (ctx.author.display_name, len(deleted), member.display_name), 
-                color=0xff1111)
+        embed = discord.Embed(
+                title="Channel purged",
+                description="**%s** removed %s messages by **%s**" % (ctx.author.display_name, len(deleted), member.display_name),
+                color=0x11ff11)
         await ctx.send(embed=embed)
         debug_print('[Bot] [Messages] User %s requested purge. Deletd %s messages from user: %s' % (ctx.author, len(deleted), member))
 
     @purge.error
     async def purge_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!purge <username> <message_amount>`')
+            embed = discord.Embed(
+                    title="Missing arguments",
+                    description="**Usage:**  `n!purge <username> <message_amount>`",
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(':warning: **Member not found. Make sure you don\'t use nicknames.**')
+            embed = discord.Embed(
+                    title="Member not found",
+                    description="**Make sure you don\'t use nicknames**",
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
-            debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
+            embed = discord.Embed(
+                    title="Missing permissions",
+                    description="**You don\'t have the permissions to do that**",
+                    color=0xff1111)
+            await ctx.send(embed=embed)
+            debug_print('[Bot] [E] [Messages] Could not parse purge arguments for user: %s' % ctx.author)
         else:
             error_print(error)
 
@@ -51,9 +68,13 @@ class MessagesCog(commands.Cog):
 
     @commands.command()
     @commands.check_any(commands.is_owner(), check_whitelist())
-    async def spam(self, ctx, amount : int, *, message2send : str):  #TODO
+    async def spam(self, ctx, amount : int, *, message2send : str):
         if amount < 1:
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!spam <ammount> <message>`')
+            embed = discord.Embed(
+                    title="Missing arguments",
+                    description="**Usage:**  `n!spam <ammount> <message>`",
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Messages] Could not parse negative integer in spam for user: %s' % ctx.author)
             return
 
@@ -65,10 +86,18 @@ class MessagesCog(commands.Cog):
     @spam.error
     async def spam_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(':warning: **Missing required arguments. Usage:**  `n!spam <amount> <message>`')
-            debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
+            embed = discord.Embed(
+                    title="Missing arguments",
+                    description="**Usage:**  `n!spam <ammount> <message>`",
+                    color=0xff1111)
+            await ctx.send(embed=embed)
+            debug_print('[Bot] [E] [Messages] Could not parse spam arguments for user: %s' % ctx.author)
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send(':warning: **You don\'t have the permissions to do that, %s.**' % ctx.author.mention)
+            embed = discord.Embed(
+                    title="Missing permissions",
+                    description="**You don\'t have the permissions to do that**",
+                    color=0xff1111)
+            await ctx.send(embed=embed)
             debug_print('[Bot] [E] [Messages] Could not parse arguments for user: %s' % ctx.author)
         else:
             error_print(error)
